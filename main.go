a9564659ed3f83686a0dc9020f650a6ebe3ce863
StrongDonald <topdev19991228@gmail.com>
2023-09-18 18:02:52 -0700

    added openapi specs

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
+// Recipes API
+//
+// This is a sample recipes API. You can find out more about the API at https://github.com/PacktPublishing/BuildingDistributed-Applications-in-Gin.
+//
+// Schemes: http
+// Host: localhost:8080
+// BasePath: /
+// Version: 1.0.0
+// Consumes:
+// - application/json
+// Produces:
+// - application/json
+// swagger:meta
 package main
 
 import (
@@ -51,6 +64,14 @@ func NewRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+// swagger:operation GET /recipes recipes listRecipes
+// Returns list of recipes
+// ---
+// produces:
+// - application/json
+// responses:
+// '200':
+// description: Successful operation
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
